94a72e8a8f093fa8c6bd2d3b0123171bec7d5487
Jeremiah <jaythinkshappiness@gmail.com>
2017-02-17 09:43:15 -0800

    add input box in Input.html.twig , setup routing to that page via app.php , create routing to app/app.php in web folder-index.php

--- app/app.php
+++ app/app.php
@@ -8,7 +8,13 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'  => __DIR__.'/../views'));
 
-    
+    $app->get("/" , function() use ($app)
+    {
+        return $app['twig']->render('Input.html.twig');
+    });
 
+
+
+    return $app;
 
 ?>
