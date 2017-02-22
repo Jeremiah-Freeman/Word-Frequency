f3e6d7e0ec7fbc5f456000ecfd42dd0e1c06c34e
Jay Freeman <jaythinkshappiness@gmail.com>
2017-02-22 00:21:35 -0800

    add vendor folder to .gitignore

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,15 +4,53 @@
         function findTheWord($input2,$input)
         {
             $input = (string)strtolower($input);
-            $input2 = (string)strtolower($input2);
+            $input2 = explode(" " , $input2);
+            // var_dump($input2);
 
-            $count = substr_count($input2,$input);
-            return $count;
+            $count = 0;
+
+            // $count = substr_count($input2,$input);
+            for($i = 0; $i <= count($input2); $i++) {
+                if($input2[$i] == $input) {
+                    $test+=$input2[$i];
+
+
+                        echo  "yay";
+
+                    // return $count;
+                    // foreach($test as $tester => $value){
+
+                    // }
+                }
+
+
+
+
+            }
 
         }
-    };
+    }
 
 
 
+
+?>
+<?php
+    class RepeatCounter
+    {
+        function findTheWord($input2,$input)
+        {
+            $input = ($input);
+            $input2 = explode(" " , $input2);
+            $input2 = array_map('strtolower', $input2);
+            $count = 0;
+            for($i = 0; $i <= count($input2); $i++) {
+                if($input2[$i] == $input) {
+                    $test+=$input2[$i];
+                    $count++;
+                }
+            }return $count;
+        }
+    }
 
 ?>
